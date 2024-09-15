5740a1c0c606f120042870d242888e3156ffb737
maybe-skiFF <den.skiff190@gmail.com>
2024-09-15 22:53:13 +0300

    feat: add userAvatarSection

---
+++ src/components/UserAvatarSection/UserAvatarSection.tsx
@@ -0,0 +1,22 @@
+import styles from './UserAvatarSection.module.css';
+import userAvatar from '../../../public/user-avatar-big.png';
+
+const UserAvatarSection = () => {
+  return (
+    <div className={styles.userAvatarSectionWrapper}>
+      <img className={styles.userAvatarImg} src={userAvatar} alt="userAvatar" />
+      <div>
+        <p
+          className={`${styles.userDescription} ${styles.userDescriptionActive}`}
+        >
+          Данные профиля
+        </p>
+        <p className={styles.userDescription}>Рабочее пространство</p>
+        <p className={styles.userDescription}>Приватность</p>
+        <p className={styles.userDescription}>Безопасность</p>
+      </div>
+    </div>
+  );
+};
+
+export { UserAvatarSection };
--- src/pages/UserPage/UserPage.tsx
+++ src/pages/UserPage/UserPage.tsx
@@ -1,10 +1,22 @@
 import styles from './UserPage.module.css';
 import { Header } from '../../components/Header/Header';
-
+import arrowLeft from '../../../public/arrow-left.png';
+import { UserAvatarSection } from '../../components/UserAvatarSection/UserAvatarSection';
 const UserPage = () => {
   return (
     <div className={styles.userPageWrapper}>
       <Header />
+      <div className={styles.userPageContent}>
+        <button className={styles.backBtn}>
+          <img
+            className={styles.arrowLeftImg}
+            src={arrowLeft}
+            alt="arrowLeft"
+          />
+          Назад
+        </button>
+        <UserAvatarSection />
+      </div>
     </div>
   );
 };
